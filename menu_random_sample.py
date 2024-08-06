2a0c2e2422ae39246d5876b9557280338b2322e7
Sejong Yang <kingsj0405@gmail.com>
2024-08-07 03:56:33 +0900

    Update menu_random_sample.py
    Implement to use AWS SQS for the quick response to slack slash command

--- menu_random_sample.py
+++ menu_random_sample.py
@@ -1,41 +1,58 @@
+import os
+import logging
+import boto3
 import random
 import json
 import requests
 import urllib.parse
 import urllib.request
-import os
 from dotenv import load_dotenv
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 
-# Load environment variables
-load_dotenv()
-DEFAULT_RESPONSE_URL = os.getenv("DEFAULT_RESPONSE_URL")
-
-SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
-SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
-RANGE_NAME = os.getenv("RANGE_NAME")
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-
 def lambda_handler(event, context):
-    # Get the slack response url and sample condition
-    print(f"[DEBUG] event: {event}")
-    print(f"[DEBUG] context: {context}")
     if 'body' in event:
+        logging.info("The slash command invoke lambda")
+        # Response to the slack
         body = urllib.parse.parse_qs(event['body'])
-        print(f"[DEBUG] body: {body}")
+        logging.debug(f"body: {body}")
+        response_url = body.get('response_url', None)[0]
+        assert response_url is not None, "The response_url is not in the body"
+        requests.post(response_url, data=json.dumps({
+            'text': "처리 중 입니다. :alarm_clock:"
+        }), headers={'Content-Type': 'application/json'})
+        # Queue to SQS
+        SQS = boto3.client('sqs')
+        SQS.send_message(
+            QueueUrl=os.getenv("SQS_QUERY_URL"),
+            MessageBody=event['body']
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps('The query is queued to SQS')
+        }
+    # Load environment variables
+    load_dotenv()
+    DEFAULT_RESPONSE_URL = os.getenv("DEFAULT_RESPONSE_URL")
+    SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
+    SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+    RANGE_NAME = os.getenv("RANGE_NAME")
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    if 'Records' in event:
+        logging.info("SQS invoke the lambda")
+        body = urllib.parse.parse_qs(event['Records'][0]['body'])
+        logging.debug(f"body: {body}")
         response_url = body.get('response_url', DEFAULT_RESPONSE_URL)[0]
-        category = body.get('text', '')[0]
+        category = body.get('text', [''])[0]
     else:
-        print(f"[INFO] no body in event, maybe AWS EventBridge invoke the lambda")
+        logging.info(f"no body in event, maybe AWS EventBridge invoke the lambda")
         response_url = DEFAULT_RESPONSE_URL
         category = ''
-    print(f"[INFO] response_url: {response_url}")
-    print(f"[INFO] category: {category}")
+    logging.info(f"response_url: {response_url}")
+    logging.info(f"category: {category}")
     # Get the menu list from google spreadsheets
-    print(f"[INFO] try to get menu list from google spreadsheets")
+    logging.info("try to get menu list from google spreadsheets")
     creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
@@ -58,15 +75,15 @@ def lambda_handler(event, context):
         else:
             probs.append(1)
     probs = [p / sum(probs) for p in probs]
-    print(f"[INFO] menus: {menus}")
-    print(f"[INFO] probs: {probs}")
+    logging.info(f"[INFO] menus: {menus}")
+    logging.info(f"[INFO] probs: {probs}")
     # Random sample menus and generate slack message
     if len(menus) == 0:
         slack_message = "조건에 맞는 메뉴가 없습니다. CIPLAB_menu_list를 확인하거나 학식이나 먹으러 가시죠 :ciplab_party:"
     else:
         emoji_map = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:', ':nine:']
         selected_menu = random.sample(menus, min(5, len(menus)))
-        print(f"[INFO] selected_menu: {selected_menu}")
+        logging.info(f"[INFO] selected_menu: {selected_menu}")
         slack_message = f"오늘의 메뉴 추천은 "
         for i, sm in enumerate(selected_menu):
             if i == 0:
@@ -81,6 +98,6 @@ def lambda_handler(event, context):
     # Return the response
     return {
         'statusCode': 200,
-        'body': json.dumps(f'Message sent to Slack!\nMessage Content\n{slack_message}')
+        'body': json.dumps('The selected menu is sent to Slack!')
     }
     
